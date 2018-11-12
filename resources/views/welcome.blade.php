1fa5142a3466b5ba0b4dc30d429895dd8236df4b
vgvineetgpt <vgvineetgpt@gmail.com>
2018-11-12 17:38:05 +0530

    ayushdfgh change-12-Nov-18

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Mastech Digital</title>
+        <title>Mastech Digitall</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
